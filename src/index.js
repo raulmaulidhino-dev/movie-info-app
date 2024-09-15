9345dcacae68e7daa414f2755ff5dc2b8c3c4059
IsnaJesika <jesikaparasinta@gmail.com>
2024-09-15 22:59:50 +0700

    eslint

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import express from 'express';
 import path from 'node:path';
 import http from 'node:http';
 import mainRouter from './router.js'; // Mengimpor router utama
-=======
-import express from "express";
-import path from "node:path";
-import http from "node:http";
-import mainRouter from "./router.js"; // Mengimpor router utama
->>>>>>> 4d0e19ad92b24ae7928af6ad3d7504bdaef55685
 
 const app = express();
 const PORT = 3000;
@@ -23,10 +16,6 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 
 const server = http.createServer(app);
 
-server.listen(PORT, 'localhost', function(){
-<<<<<<< HEAD
+server.listen(PORT, 'localhost', function () {
   console.log('Listening on localhost:3000');
-=======
-    console.log('Listening on localhost:3000');
->>>>>>> 4d0e19ad92b24ae7928af6ad3d7504bdaef55685
 });
