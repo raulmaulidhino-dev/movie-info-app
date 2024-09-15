8333bf245760d27a4f1897d128dca8ed9770e41e
Raul Maulidhino <raulahmadm314@gmail.com>
2024-09-15 21:48:56 +0700

    resolve some conflicts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import express from 'express';
 import path from 'node:path';
 import http from 'node:http';
 import mainRouter from './router.js'; // Mengimpor router utama
-=======
-import express from "express";
-import path from "node:path";
-import http from "node:http";
-import mainRouter from "./router.js"; // Mengimpor router utama
->>>>>>> 4d0e19ad92b24ae7928af6ad3d7504bdaef55685
 
 const app = express();
 const PORT = 3000;
@@ -24,9 +17,5 @@ app.use('/static', express.static(path.join(__dirname, 'public')));
 const server = http.createServer(app);
 
 server.listen(PORT, 'localhost', function(){
-<<<<<<< HEAD
   console.log('Listening on localhost:3000');
-=======
-    console.log('Listening on localhost:3000');
->>>>>>> 4d0e19ad92b24ae7928af6ad3d7504bdaef55685
 });
